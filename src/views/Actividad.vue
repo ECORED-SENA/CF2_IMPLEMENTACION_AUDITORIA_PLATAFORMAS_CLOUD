98a60e7c85a47ef4c96984dd1955244e26add8f3
Andrea Botello <andrea.botellor@misena.edu.co>
2022-04-27 14:11:26 -0500

    updated

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,17 +7,17 @@
       .col-8.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.px-md-3
           figure
-            img(src="@/assets/curso/images/sopa-letras.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/curso/images/seleccion-multiple.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Sopa de Letras
+          h2 Selección múltiple
         p.mb-4 Afianzar algunos de los  conceptos más importantes de la arquitectura, servicios y roles asociados a la computación en la nube.
         .tarjeta.tarjeta--azul.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Buscar las palabras ocultas en la sopa de letras
+              p.fw-bold.mb-0 Identificar la respuesta correcta
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
